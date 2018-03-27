60f90624a4b3b2a27cd75b65eaea29e86179a126
Unknown <szlejter.a@gmail.com>
2018-03-27 13:16:32 +0200

    All

---
+++ script.js
@@ -0,0 +1,11 @@
+ const bar = document.querySelector(".status");
+
+const statusBar = () => {
+    currentScroll = Math.round(window.scrollY / (document.documentElement.offsetHeight - window.innerHeight) * 100); 
+    
+   // bar.style.width = currentScroll + "%";
+   bar.style.width = currentScroll + "%";
+}
+
+
+window.addEventListener("scroll", statusBar)  
